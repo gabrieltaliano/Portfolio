72d4c6aeb539172e3e557ec21788ff59410a5b1a
Gabriel Taliano <gabrieltaliano@gmail.com>
2023-05-05 14:13:28 -0300

    API key update

--- stack.ts
+++
@@ -1,3 +0,0 @@
-const stack = ['AWS', 'Algolia', 'Arduino', 'CSS', 'Chai', 'Datadog', 'Docker', 'EC2', 'ElasticSearch', 'Esxi', 'Express', 'Field Support', 'Firebase', 'Firestore', 'GCP', 'Git', 'HTML5', 'Help Desk', 'IIS', 'ITIL Service Management', 'Information Technology Service Management', 'JavaScript', 'Jest', 'Jira', 'Linux', 'Lodash', 'Material UI', 'MathJs', 'Micro frontend', 'Mocha', 'NextJs', 'Node', 'Node.JS', 'Photoshop', 'Powershell', 'QuillJs', 'Re charts', 'React', 'React Native', 'React Test Library', 'ReactJs', 'Recharts', 'Redis', 'Redux', 'SCCM', 'Scripting', 'Server Management', 'SocketIO', 'Sqlite', 'Storage Services', 'Storybook', 'SysAdmin', 'Tailwinds', 'Typescript', 'Unix', 'VMware', 'Virtualization', 'Vsphere', 'Windows Server'];
-
-export default stack;
