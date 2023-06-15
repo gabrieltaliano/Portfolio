e07247f91227e78ce4d79fe3d08f2bb271d5b1da
Gabriel Taliano <gabrieltaliano@gmail.com>
2023-06-16 01:06:48 +1200

    Add Pixel perfect project

--- Components/MyWork/projectsList.ts
+++ Components/MyWork/projectsList.ts
@@ -5,115 +5,127 @@ import yb_mobile from "../../public/images/yb-mobile.jpg";
 import tb from "../../public/images/tb.jpg";
 import rk from "../../public/images/rk-b.jpg";
 import mediafly from "../../public/images/mediafly.jpg";
+import pixelPerfect from "../../public/images/pixelPerfect.jpg";
 import {
-  algolia,
-  bootstrap,
-  emailJS,
-  firebaseAuth,
-  firebaseFirestore,
-  formik,
-  i18n,
-  komoju,
-  next_tech,
-  node_tech,
-  postcss,
-  reactNative,
-  react_tech,
-  redis,
-  redux,
-  restApi,
-  stripe,
-  svg,
-  tailwind,
-  typeScript,
-  vercel,
-  javascript,
+    algolia,
+    bootstrap,
+    emailJS,
+    firebaseAuth,
+    firebaseFirestore,
+    formik,
+    i18n,
+    komoju,
+    next_tech,
+    node_tech,
+    postcss,
+    reactNative,
+    react_tech,
+    redis,
+    redux,
+    restApi,
+    stripe,
+    svg,
+    tailwind,
+    typeScript,
+    vercel,
+    javascript
 
 } from "./technologies";
 
 const projects: work[] = [
-  {
-    name: "Mediafly",
-    url: "https://www.mediafly.com/",
-    img: [{ src: mediafly.src, orientation: "h" }],
-    description: `• API integration project in Javascript
+    {
+        name: "Mediafly",
+        url: "https://www.mediafly.com/",
+        img: [{ src: mediafly.src, orientation: "h" }],
+        description: `• API integration project in Javascript
 • Query modeling for Redis with utility functions
 • Angular to React migration (micro-frontend)
 • Development of visualization modules in react and recharts`,
-    stack: [
-      javascript,
-      restApi,
-      redis,
-      redux,
-      react_tech
-    ],
-  },
-  {
-    name: "Ticket Bundle",
-    // url: "https://ticket-bundle.vercel.app/",
-    img: [{ src: tb.src, orientation: "v" }],
-    description: `• Web application for creating and monitoring ticket packages for promotions, discounts and/or event tickets.
-    • Rest API for status queries
-    • Developed in Next.js, with TypeScript, SVG graphics, Firebase Auth and Firestore.`,
-    stack: [
-      typeScript,
-      restApi,
-      firebaseFirestore,
-      firebaseAuth,
-      vercel,
-      postcss,
-      svg
-    ],
-  },
-  {
-    name: "Mitsushio Welding",
-    description: `• Web application development to authorize and manage access roles.
+        stack: [
+            javascript,
+            restApi,
+            redis,
+            redux,
+            react_tech
+        ],
+    },
+    {
+        name: "Perfect Pixel",
+        url: "https://perfect-pixel-gabrieltaliano.vercel.app/",
+        img: [{ src: pixelPerfect.src, orientation: "h" }],
+        description: `Small site to see different css units in action (in progress)`,
+        stack: [
+            tailwind,
+            vercel,
+            react_tech
+        ],
+    },
+    {
+        name: "Mitsushio Welding",
+        description: `• Web application development to authorize and manage access roles.
     • Internal Web application development to authorize and manage access roles with React, Firebase Functions (node.js) and Firebase Auth.
     • Corporate portal renewal in next.js`,
-    // url: "https://mitsushio-welding.com/",
-    img: [{ src: mw.src, orientation: "h" }],
-    stack: [bootstrap, emailJS, firebaseFirestore, firebaseAuth, react_tech],
-  },
-  {
-      name: "YB Languages",
-      url: "https://delfo-yb.web.app/",
-    description: `• Website developed in Next.js with focus in internationalization with i18n
+        // url: "https://mitsushio-welding.com/",
+        img: [{ src: mw.src, orientation: "h" }],
+        stack: [bootstrap, emailJS, firebaseFirestore, firebaseAuth, react_tech],
+    },
+    {
+        name: "YB Languages",
+        url: "https://delfo-yb.web.app/",
+        description: `• Website developed in Next.js with focus in internationalization with i18n
 • Komoju/Stripe payment gateway with Node backend
 • Dashboard to manage shared files and assign roles to users
 • Google Cloud Platform hosted storage`,
-    img: [
-      { src: yb_pc.src, orientation: "h" },
-      { src: yb_mobile.src, orientation: "v" },
-    ],
-    stack: [
-      next_tech,
-      tailwind,
-      i18n,
-      firebaseAuth,
-      firebaseFirestore,
-      formik,
-      algolia,
-      typeScript,
-      node_tech,
-      komoju,
-      stripe,
-    ],
-  },
-  {
-    name: "My Random Kana",
-    url: "https://myrandomkana.web.app/",
-    img: [{ src: rk.src, orientation: "v" }],
-    description: `Game for memorice japanese alphabet. React native version in progress...`,
-    stack: [
-      react_tech,
-      typeScript,
-      firebaseAuth,
-      { ...reactNative, note: "(in progress)" },
-      redux,
-      { ...redis, note: "(migrating)" },
-      postcss,
-    ],
-  },
+        img: [
+            { src: yb_pc.src, orientation: "h" },
+            { src: yb_mobile.src, orientation: "v" },
+        ],
+        stack: [
+            next_tech,
+            tailwind,
+            i18n,
+            firebaseAuth,
+            firebaseFirestore,
+            formik,
+            algolia,
+            typeScript,
+            node_tech,
+            komoju,
+            stripe,
+        ],
+    },
+    {
+        name: "My Random Kana",
+        url: "https://myrandomkana.web.app/",
+        img: [{ src: rk.src, orientation: "v" }],
+        description: `Game for memorice japanese alphabet. React native version in progress...`,
+        stack: [
+            react_tech,
+            typeScript,
+            firebaseAuth,
+            { ...reactNative, note: "(in progress)" },
+            redux,
+            { ...redis, note: "(migrating)" },
+            postcss,
+        ],
+    },
+    {
+        name: "Ticket Bundle",
+        // url: "https://ticket-bundle.vercel.app/",
+        img: [{ src: tb.src, orientation: "v" }],
+        description: `• Web application for creating and monitoring ticket packages for promotions, discounts and/or event tickets.
+    • Rest API for status queries
+    • Developed in Next.js, with TypeScript, SVG graphics, Firebase Auth and Firestore.`,
+        stack: [
+            typeScript,
+            restApi,
+            firebaseFirestore,
+            firebaseAuth,
+            vercel,
+            postcss,
+            svg
+        ],
+    }
 ];
 
 export default projects;
