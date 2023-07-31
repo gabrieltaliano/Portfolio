f0e762d45959c6c574ee05ee13ef9eaf0302db1b
Gabriel Taliano <gabrieltaliano@gmail.com>
2023-07-31 13:25:48 +1200

    CV route

---
+++ pages/cv.tsx
@@ -0,0 +1,10 @@
+export default function CV() {
+  return (
+    <div className="w-full flex justify-center">
+      <a className="text-green-400 p-8"
+        href="https://drive.google.com/drive/folders/1VDyuokBaCYG2uVm5UPFZaSH44ci2Libz?usp=sharing"
+        target="blanc"
+      >My CV</a>
+    </div>
+  );
+}
